fa5bb22d2183dd350c8fb5d8d83d41aec0b9dd10
Oleh <olvolo17@gmail.com>
2024-06-22 21:58:34 +0300

    Modify class JavaConfig and JavaConfigService with @Qualifier and @Primary

--- src/main/java/org/example/springbeans/config/JavaConfig.java
+++ src/main/java/org/example/springbeans/config/JavaConfig.java
@@ -3,18 +3,26 @@ package org.example.springbeans.config;
 import org.example.springbeans.service.JavaConfigService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.OffsetDateTime;
 
 @Configuration
 public class JavaConfig {
 
-    @Bean
-    public JavaConfigService javaConfigService(){
+    @Bean("FullJavaConfigService")
+    @Primary
+    public JavaConfigService javaConfigService() {
         OffsetDateTime offsetDateTime = OffsetDateTime.now();
         JavaConfigService javaConfigService = new JavaConfigService(offsetDateTime.toString());
         javaConfigService.setDayOfMonth(offsetDateTime.getDayOfMonth());
         return javaConfigService;
+    }
+
+    @Bean("PureJavaConfigService")
+    public JavaConfigService javaConfigService1() {
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+        return new JavaConfigService(offsetDateTime.toString());
     }
 
 }
--- src/main/java/org/example/springbeans/controller/JavaConfigController.java
+++ src/main/java/org/example/springbeans/controller/JavaConfigController.java
@@ -2,6 +2,7 @@ package org.example.springbeans.controller;
 
 import org.example.springbeans.service.JavaConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,15 @@ public class JavaConfigController {
 
     private final JavaConfigService javaConfigService;
 
+//    @Autowired
+//    public JavaConfigController(@Qualifier("PureJavaConfigService") JavaConfigService javaConfigService) {
+//        this.javaConfigService = javaConfigService;
+//    }
+
     @Autowired
     public JavaConfigController(JavaConfigService javaConfigService) {
         this.javaConfigService = javaConfigService;
     }
-
     @GetMapping("java")
     public String hello() {
         return javaConfigService.hello();
